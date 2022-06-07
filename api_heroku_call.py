0bf3a3ea7a1b5f53d5ac1430b83cab50cd764273
Mateus Cichelero <eng.cichelero@gmail.com>
2022-06-06 21:08:32 -0300

    feat:Updating Procfile

---
+++ api_heroku_call.py
@@ -0,0 +1,26 @@
+"""
+Heroku Api test script
+"""
+import requests
+
+data = {
+            'age': 35,
+            'workclass': 'Private',
+            'fnlgt': 149184,
+            'education': 'Masters',
+            'marital_status': 'Divorced',
+            'occupation': 'Prof-specialty',
+            'relationship': 'Wife',
+            'race': 'White',
+            'sex': 'Female',
+            'hoursPerWeek': 58,
+            'nativeCountry': 'United-States'
+    }
+
+
+r = requests.post('https://census-salaries-prediction.herokuapp.com/', json=data)
+
+assert r.status_code == 200
+
+print("Response code: %s" % r.status_code)
+print("Response body: %s" % r.json())
